73e2f320df3213e6cda2677721b4198b165cd5ec
Enrique Querol <enrique.eqg@gmail.com>
2018-02-22 12:45:28 +0100

    First config.

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,16 +1,15 @@
-import * as functions from 'firebase-functions';
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const functions = require("firebase-functions");
 // import * as admin from "firebase-admin";
-
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Hello from Firebase!");
 });
-
 // const config = functions.config();
 // admin.initializeApp(config.firebase);
-
 /*
   Send email alert to admin when a contact message is sent.
 */
@@ -22,3 +21,4 @@ export const helloWorld = functions.https.onRequest((request, response) => {
 //     throw new Error(error);
 //   }
 // });
+//# sourceMappingURL=index.js.map
